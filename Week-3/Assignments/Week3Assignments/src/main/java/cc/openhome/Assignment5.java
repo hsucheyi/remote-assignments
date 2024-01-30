0a8a4e55fbe3f4398004eb8235e5efbabe69c292
Cheyi Hsu <152784506+hsucheyi@users.noreply.github.com>
2024-01-30 15:18:47 +0800

    Adjust assignment5 display

--- Week-3/Assignments/Week3Assignments/src/main/java/cc/openhome/Assignment5.java
+++ Week-3/Assignments/Week3Assignments/src/main/java/cc/openhome/Assignment5.java
@@ -2,23 +2,23 @@ package cc.openhome;
 
 public class Assignment5 {
 
-	public static void main(String[] args) {
-		int[] nums = {2, 7, 11, 15};
+    public static void main(String[] args) {
+	int[] nums = {2, 7, 11, 15};
         int target = 9;
         int[] result = twoSum(nums, target);
 
         System.out.println("[" + result[0] + ", " + result[1] + "]");
-	}
+    }
 	
-	 public static int[] twoSum(int[] nums, int target) {
-		 for (int i = 0; i < nums.length - 1; i++) {
-			 for (int j = i + 1; j < nums.length; j++) {
-				 if (nums[i] + nums[j] == target) {
-					 return new int[]{i, j};
-				 }
-			 }
-		 }
-	     return new int[]{};
-	 }
+    public static int[] twoSum(int[] nums, int target) {
+	for (int i = 0; i < nums.length - 1; i++) {
+	    for (int j = i + 1; j < nums.length; j++) {
+		if (nums[i] + nums[j] == target) {
+		    return new int[]{i, j};
+		}
+	    }
+	}
+	return new int[]{};
+    }
 	 
 }
